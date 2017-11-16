367f702e1db4b8a7a23d85b13e48a1968e56bec7
Kyle <kapkap20@live.co.uk>
2017-11-16 17:16:57 +0000

    Wizards Assembly
    Digital Assignment By Kyle Papachristophorou

---
+++ js/init.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.carousel').carousel();
+
+      
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+      
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+
